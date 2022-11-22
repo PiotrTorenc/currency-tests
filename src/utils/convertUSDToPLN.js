5ec77d6b559795ec1e738423615130b4312ce7b2
PiotrTorenc <piotr.torenc@gmail.com>
2022-11-22 15:36:23 +0100

    add tests

--- src/utils/convertUSDToPLN.js
+++ src/utils/convertUSDToPLN.js
@@ -1,9 +1,16 @@
 export const convertUSDToPLN = (USD) => {
+  if (typeof USD === 'string' || USD === undefined) {
+    return NaN;
+  } else if (typeof USD !== 'number') {
+    return 'Error';
+  } else if (USD <= 0) {
+    USD = 0;
+  }
   const USDtoPLN = USD * 3.5;
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
-    currency: 'PLN'
+    currency: 'PLN',
   });
 
-  return formatter.format(USDtoPLN).replace(/\u00a0/g, ' ');
-}
+  return formatter.format(USDtoPLN);
+};
